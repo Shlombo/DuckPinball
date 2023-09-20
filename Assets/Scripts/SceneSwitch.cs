3ffe21a2b750294613bb139cafd4f4989194d430
David Wang <ywang146@ncsu.edu>
2023-09-20 07:33:40 -0400

    adding logic to title page

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Application.LoadLevel("Main Scene");
+        }
+    }
+}
